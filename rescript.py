85457d109adde2f22310b7b580a1cfd3f8626506
krystianopryszek99 <kopryszek@gmail.com>
2021-07-20 12:12:28 +0100

    Added Thompson's construction

--- rescript.py
+++ rescript.py
@@ -49,6 +49,141 @@ def shunt(infix):
     # Return the postfix version of infix
     return postfix
 
+# Thompson's construction
+
+class State:
+    """ A state and it's arrows in Thompson's construction. """
+    def __init__(self, label, arrows, accept):
+        """ Label is the arrow  labels, arrows is a list of states to point to, accept is a boolean as to whether this is an accept state """
+        self.label = label
+        self.arrows = arrows
+        self.accept = accept
+
+    # Method that returns a set of states that are from the following this states and all its e arrows
+    def followes(self):
+        """ Returns sets of states that are from the following thus state and all its e arrows """
+        # Include this state in the returned set
+        states = {self}
+        # if this state has e arrows
+        if self.label is None: 
+            # loop through this state's arrows
+            for state in self.arrows:
+                states = (states | state.followes())
+        # returns the set of states
+        return states
+
+class NFA:
+    """ A non-deterministic finite automaton """
+    def __init__(self, start, end):
+      # has a start and end state
+      self.start = start
+      self.end = end
+
+    # method taking a self as first argument
+    # method for matching 
+    def match(self, s):
+        """" Return True if the NFA mathces the string s """
+        # list of previous states
+        previous = self.start.followes()
+        # loop through the string 
+        for c in s:
+            # start with an empty set 
+            current = set()
+            # loop through the previous states
+            for state in previous:
+                # check if there is a c arrow from state
+                if state.label == c:
+                    # add followes for next state
+                    current = (current | state.arrows[0].followes())
+            # replace previous with current
+            previous = current
+        # if the final state is in previous, return true
+        return (self.end in previous)
+
+def re_to_nfa(postfix):
+    # A stack for NFA's
+    stack = []
+    # Loop through the postfix r.e left to right.
+    for c in postfix:
+        # Concatenation
+        if c == '.':
+            # Pop top NFA off stack
+            nfa2 = stack[-1]
+            stack = stack[:-1]
+            # Pop the next NFA off stack
+            nfa1 = stack[-1]
+            stack = stack[:-1]
+            # Make accept state of NFA1 non-accept
+            nfa1.end.accept = False
+            # Make it point at start state of nfa2
+            nfa1.end.arrows.append(nfa2.start)
+            # Make a new NFA with nfa1's start state and nfa2's end state
+            nfa = NFA(nfa1.start, nfa2.end)
+            # Push to the stack
+            stack.append(nfa)
+        elif c == '|':
+            # Pop top NFA off stack
+            nfa2 = stack[-1]
+            stack = stack[:-1]
+            # Pop the next NFA off stack
+            nfa1 = stack[-1]
+            stack = stack[:-1]
+            # Create new start and end states
+            start = State(None, [], False)
+            end = State(None, [], True)
+            # Make new start state point at old start states
+            start.arrows.append(nfa1.start)
+            start.arrows.append(nfa2.start)
+            # Make old end states non-accept
+            nfa1.end.accept = False
+            nfa2.end.accept = False
+            # Point old end states to new one
+            nfa1.end.arrows.append(end)
+            nfa2.end.arrows.append(end)
+            # Make a new NFA 
+            nfa = NFA(start, end)
+            # Push to the stack
+            stack.append(nfa)
+        elif c =='*':
+              # Pop one NFA off stack
+            nfa1 = stack[-1]
+            stack = stack[:-1]
+            # Create new start and end states
+            start = State(None, [], False)
+            end = State(None, [], True)
+            # Make new start state point at old start states
+            start.arrows.append(nfa1.start)
+            # And at the new end state
+            start.arrows.append(end)
+            # Make old accept state non-accept
+            nfa1.end.accept = False
+            # Make old end state point to new end state
+            nfa1.end.arrows.append(end)
+            # Make old accept state point to old start state
+            nfa1.end.arrows.append(nfa1.start)
+            # Make a new NFA 
+            nfa = NFA(start, end)
+            # Push to the stack
+            stack.append(nfa)
+        else:
+            # Create an NFA for the non-special character c
+            # Create the end state
+            end = State(None, [], True)
+            # Create the start state
+            start = State(c, [], False)
+            # Point new start state at new end state
+            start.arrows.append(end)
+            # Create the NFA with the start and end state
+            nfa = NFA(start, end)
+            # Append the NFA to the NFA stack
+            stack.append(nfa)
+    
+    # There should only be one NFA on the stack
+    if len(stack) != 1:
+        return None
+    else:
+        return stack[0] 
+
 infix = "3+4*(2-1)"
 postfix = "3421-*+"
 
